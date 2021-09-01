af528996c01daa0f3948c8057837587b2b01402b
Kevin Kamto <kamtokevin@gmail.com>
2021-09-01 16:55:18 -0400

    chore: splicing old + new versions together

--- deploy-athlete.js
+++ deploy-athlete.js
@@ -20,9 +20,9 @@ const _networkId = 80001;
 
 
 async function deployAthlete( _synthName, _synthSymbol, _expirationTimestamp, ancillaryData) {
-  const url = networkUrl;
-
+  
   // See HDWalletProvider documentation: https://www.npmjs.com/package/@truffle/hdwallet-provider.
+  const url = networkUrl;
   const hdwalletOptions = {
     mnemonic: {
       phrase: mnemonic,
@@ -31,46 +31,118 @@ async function deployAthlete( _synthName, _synthSymbol, _expirationTimestamp, an
     addressIndex: 0, // Change this to use the nth account.
   };
 
-  // Initialize web3 with an HDWalletProvider if a mnemonic was provided. Otherwise, just give it the url.
-  const web3 = new Web3(mnemonic ? new HDWalletProvider(hdwalletOptions) : url);
-  const { toWei, utf8ToHex, padRight } = web3.utils;
+  //#region web3-stuff
+    // Initialize web3 with an HDWalletProvider if a mnemonic was provided. Otherwise, just give it the url.
+    const web3 = new Web3(mnemonic ? new HDWalletProvider(hdwalletOptions) : url);
+    const { toWei, utf8ToHex, padRight } = web3.utils;
 
-  const accounts = await web3.eth.getAccounts();
-  if (!accounts || accounts.length === 0)
-    throw "No accounts. Must provide mnemonic or node must have unlocked accounts.";
-  const account = accounts[0];
-  const networkId = _networkId;
+    const accounts = await web3.eth.getAccounts();
+    if (!accounts || accounts.length === 0)
+      throw "No accounts. Must provide mnemonic or node must have unlocked accounts.";
+    const account = accounts[0];
+    const networkId = _networkId;
 
-  // Grab collateral decimals.
-  const collateral = new web3.eth.Contract(
-    getAbi("IERC20Standard"),
-    collateralToken
-  );
-  const decimals = (await collateral.methods.decimals().call()).toString();
+    // Grab collateral decimals.
+    const collateral = new web3.eth.Contract(
+      getAbi("IERC20Standard"),
+      collateralToken
+    );
+    const decimals = (await collateral.methods.decimals().call()).toString();  
+  //#endregion
 
 
-  // LSP parameters. Pass in arguments to customize these.
+
+  //#region LSP-Params
+
+    // LSP parameters. Pass in arguments to customize these.
+    const lspParams = {
+      expirationTimestamp: _expirationTimestamp, // Timestamp that the contract will expire at.
+      collateralPerPair: 10000000000000,
+      priceIdentifier: padRight(utf8ToHex("SPD"), 64), // Price identifier to use.
+      syntheticName: _synthName, // Long name.
+      syntheticSymbol: _synthSymbol, // Short name.
+      collateralToken: collateralToken, // Collateral token address.
+      financialProductLibrary: financialProductLibrary,
+      customAncillaryData: utf8ToHex(ancillaryData), // Default to empty bytes array if no ancillary data is passed.
+      prepaidProposerReward: proposerReward // Default to 0 if no prepaid proposer reward is passed.
+    };
+
+      // LSP parameters. Pass in arguments to customize these.
   const lspParams = {
-    expirationTimestamp: _expirationTimestamp, // Timestamp that the contract will expire at.
-    collateralPerPair: 10000000000000,
-    priceIdentifier: padRight(utf8ToHex("SPD"), 64), // Price identifier to use.
-    syntheticName: _synthName, // Long name.
-    syntheticSymbol: _synthSymbol, // Short name.
-    collateralToken: collateralToken, // Collateral token address.
+    pairName: argv.pairName,
+    expirationTimestamp: argv.expirationTimestamp, // Timestamp that the contract will expire at.
+    collateralPerPair: argv.collateralPerPair,  // 18 point Decimal Format
+    priceIdentifier: padRight(utf8ToHex(argv.priceIdentifier.toString()), 64), // Price identifier to use.
+    longSynthName: argv.longSynthName,
+    longSynthSymbol: argv.longSynthSymbol,
+    shortSynthName: argv.shortSynthName,
+    shortSynthSymbol: argv.shortSynthSymbol,
+    collateralToken: argv.collateralToken.toString(), // Collateral token address.
     financialProductLibrary: financialProductLibrary,
     customAncillaryData: utf8ToHex(ancillaryData), // Default to empty bytes array if no ancillary data is passed.
-    prepaidProposerReward: proposerReward // Default to 0 if no prepaid proposer reward is passed.
+    prepaidProposerReward: proposerReward, // Default to 0 if no prepaid proposer reward is passed.
+    optimisticOracleLivenessTime: livenessTime,
+    optimisticOracleProposerBond: proposerBond
   };
 
-  console.log("params:", lspParams);
+    console.log("params:", lspParams);
+    const lspCreator = new web3.eth.Contract(
+      getAbi("LongShortPairCreator"),
+      lspCreatorAddress
+    );
 
-  const lspCreator = new web3.eth.Contract(
-    getAbi("LongShortPairCreator"),
-    lspCreatorAddress
-  );
+  //#endregion
+
+  //#region FPL-Parameters
+  
+    // Set the FPL parameters.
+    if (fpl) {
+      console.log("Setting FPL parameters...");
+      // Set the deployed FPL address and lowerBound.
+      console.log("fpl address:", fpl);
+      const fplName = argv.fpl + "LongShortPairFinancialProductLibrary";
+      console.log("fpl name:", fplName);
+      const deployedFPL = new web3.eth.Contract(getAbi(fplName), fpl);
+      const lowerBound = argv.lowerBound ? argv.lowerBound : argv.strikePrice;
+      // Set parameters depending on FPL type.
+      if (argv.fpl == 'BinaryOption' || argv.fpl == 'CappedYieldDollar' || argv.fpl == 'CoveredCall' || argv.fpl == 'SimpleSuccessToken') {
+        const fplParams = [address, lowerBound];
+        console.log("fpl params:", {
+          address: fplParams[0],
+          lowerBound: fplParams[1]
+        });
+        const { transactionHash } = await deployedFPL.methods.setLongShortPairParameters(...fplParams).send(transactionOptions);
+        console.log("Financial product library parameters set in transaction:", transactionHash);
+      }
+      if (argv.fpl == 'RangeBond' || argv.fpl == 'Linear') {
+        const upperBound = argv.upperBound;
+        const fplParams = [address, upperBound, lowerBound];
+        console.log("fpl params:", {
+          address: fplParams[0],
+          upperBound: fplParams[1],
+          lowerBound: fplParams[2]
+        });
+        const { transactionHash } = await deployedFPL.methods.setLongShortPairParameters(...fplParams).send(transactionOptions);
+        console.log("Financial product library parameters set in transaction:", transactionHash);
+      }
+      if (argv.fpl == 'SuccessToken') {
+        const basePercentage = argv.basePercentage;
+        const fplParams = [address, lowerBound, basePercentage];
+        console.log("fpl params:", {
+          address: fplParams[0],
+          lowerBound: fplParams[1],
+          basePercentage: fplParams[2]
+        });
+        const { transactionHash } = await deployedFPL.methods.setLongShortPairParameters(...fplParams).send(transactionOptions);
+        console.log("Financial product library parameters set in transaction:", transactionHash);
+      }
+    }
+  
+  //#endregion
+
+
 
   console.log("network id:", networkId);
-
   // Transaction parameters
   const transactionOptions = {
     gas: 12000000, // 12MM is very high. Set this lower if you only have < 2 ETH or so in your wallet.
@@ -84,8 +156,9 @@ async function deployAthlete( _synthName, _synthSymbol, _expirationTimestamp, an
   console.log("Simulation successful. Expected Address:", address);
 
   // Since the simulated transaction succeeded, send the real one to the network.
-  const { transactionHash } = await lspCreator.methods.createLongShortPair(...Object.values(lspParams)).send(transactionOptions);
+  const { transactionHash } = await lspCreator.methods.createLongShortPair(lspParams).send(transactionOptions);
   console.log("Deployed in transaction:", transactionHash);
+  
   process.exit(0);
 }
 
